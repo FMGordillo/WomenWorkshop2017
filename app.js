5cc70f13b26933feb1f99a74a27a251ac63febee
FMGordillo <facundomgordillo@gmail.com>
2017-02-20 10:46:39 -0300

    Agregada seguridad + DB

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-require('dotenv').load();
 var express = require('express'),
   cfenv = require('cfenv'),
-  app = express();
+  app = express()
+  cloudant  = require("cloudant");
+require("./config/express")(app);
+require('dotenv').load();
 var appEnv = cfenv.getAppEnv();
+
+console.log(process.argv.join(" "));
 
 // DON'T MODIFY all above **********************************************
 
-
+app.set("testDb", true);
 
 // DON'T MODIFY all below **********************************************
+
+var usernameDb="",
+  passwordDb="",
+  dbname="Women-2017";
+
+if(process.env.VCAP_SERVICES) {
+  usernameDb=appEnv.getServiceCreds('TuTalentoOculto-cloudantNoSQLDB').username;
+  passwordDb=appEnv.getServiceCreds('TuTalentoOculto-cloudantNoSQLDB').password;
+} else {
+  usernameDb= process.env.CLOUDANT_USERNAME;
+  passwordDb= process.env.CLOUDANT_PW;
+}
+
+cloudant({
+    account: usernameDb,
+    password: passwordDb
+  }, function(err, cloudant) {
+  if (err)
+    return console.log("Error connecting to Cloudant account %s: %s", err.message);
+  if (app.get("testDb")) dbname += "_test";
+  console.log("using db:", dbname);
+   // We use this reference from now on
+  app.womendb = cloudant.use(dbname);
+});
+
+app.get("/", function(req, res) {
+  res.render("index", {
+    title: "Women Workshop",
+  });
+});
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
